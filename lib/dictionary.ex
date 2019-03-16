9830ff1c5c76656cad54cbfda9362b8e83ef181e
brandon <brandon@brandons-MacBook-Pro.local>
2019-03-15 21:31:03 -0400

    add find word count regex

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -9,4 +9,29 @@ defmodule Dictionary do
     word_list()
     |> Enum.random()
   end
+
+  def replace_cat(string) do
+    string
+    |> String.replace(~r/cat/ , "dog")
+    |> IO.puts
+  end
+
+  def find_word_regex(letter, word_count, letter_location) do
+    1..word_count
+    |> Enum.to_list
+    |> Enum.flat_map(fn number ->
+      if number !== letter_location do
+        ["."]
+      else
+        [number]
+      end
+    end)
+    |> Enum.join
+    |> IO.puts
+  end
+
+  def find_word_by_letter(letter, word_count, letter_location) do
+    word_list()
+    |> Enum.find(fn word -> word ~= ~r/a/)
+  end
 end
