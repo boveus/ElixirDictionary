867d0c14f0018b4b1b5c0c5b5d73dbf43f93472a
Brandon Stewart <brandon.stewart88@gmail.com>
2019-03-19 15:25:37 -0400

    add square and double functions

--- lib/lists.ex
+++ lib/lists.ex
@@ -1,7 +1,13 @@
 defmodule Lists do
   def len([]), do: 0
-  def len([_head | tail]), do: 1 + len(tail)
+  def len([head | tail]), do: 1 + len(tail)
 
   def sum([]), do: 0
   def sum([_head | tail]), do: _head + sum(tail)
+
+  def double([]), do: []
+  def double([head | tail]), do: [ 2*head | double(tail) ]
+
+  def square([]), do: []
+  def square([head | tail]), do: [ head * head | square(tail) ]
 end
