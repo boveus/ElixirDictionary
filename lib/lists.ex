aaab9a556466c0161da11ccb44d924da9d1059a5
Brandon Stewart <brandon.stewart88@gmail.com>
2019-03-19 15:33:26 -0400

    add sum pairs and even_length? functions

--- lib/lists.ex
+++ lib/lists.ex
@@ -1,3 +1,4 @@
+require Integer
 defmodule Lists do
   def len([]), do: 0
   def len([head | tail]), do: 1 + len(tail)
@@ -10,4 +11,14 @@ defmodule Lists do
 
   def square([]), do: []
   def square([head | tail]), do: [ head * head | square(tail) ]
+
+  def sum_pairs([]), do: []
+  def sum_pairs([h1, h2 | t]), do: [h1 + h2 | sum_pairs(t)]
+
+  def even_length?([]), do: false
+  def even_length?([head | tail]) do
+      [head | tail]
+      |> len()
+      |> Integer.is_even()
+  end
 end
